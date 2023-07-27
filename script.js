01d96c43a172c2d6bc4b17e36bde388a5326560f
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-07-27 11:27:19 +0300

    style changes

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const target = 30;
+const target = 1;
 let clickedElements = [];
 
-const popupElement = document.querySelector(".popup");
+const popupElement = document.querySelector(".win-popup");
 const gameOverElement = document.querySelector(".game-over");
-const closePopupButton = document.querySelector(".popup .game-nav-button");
+const closePopupButton = document.querySelector(".win-popup .game-nav-button");
 
-// const popup
-const coronaVirusElementsList = document.querySelectorAll(".body:not(.obstacle)");
-coronaVirusElementsList.forEach(function (element) {
+const infectedElementsList = document.querySelectorAll(".body:not(.obstacle)");
+infectedElementsList.forEach(function (element) {
   element.addEventListener("click", function () {
     if (clickedElements.find((elem) => elem == element)) {
       return;
     }
     clickedElements.push(element);
     if (clickedElements.length == target) {
-      gameOverElement.style.animationState = "paused";
       popupElement.style.display = "block";
     }
   });
@@ -23,3 +21,4 @@ coronaVirusElementsList.forEach(function (element) {
 closePopupButton.addEventListener("click", function () {
   popupElement.style.display = "none";
 });
+
