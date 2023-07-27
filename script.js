f48e599460e96fa4602a6291a55230386b362eee
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-07-27 11:50:34 +0300

    ..

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const target = 30;
 let clickedElements = [];
 
-document.addEventListener("click", function (eInfo) {
-  eInfo.preventDefault();
-});
-
 const popupElement = document.querySelector(".win-popup");
 const gameOverElement = document.querySelector(".game-over");
 const closePopupButton = document.querySelector(".win-popup .game-nav-button");
